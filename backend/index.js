795b5a70f03a92a98120239ac370d21ff9112aec
Ronak Patel <ronak2715@gmail.com>
2022-10-29 22:16:03 -0400

    Final

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const { Sequelize } = require('sequelize')
 
 // Express Settings
 app.use(cors())
@@ -11,12 +12,27 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+
+
+
+  
+
 // Controllers & Routes
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
